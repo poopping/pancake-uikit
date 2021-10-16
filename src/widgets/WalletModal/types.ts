1fec9d76df63deb5852d7744d9f08e2acdb0ed21
b <bhtruong@hotmail.com>
2021-10-15 20:49:09 -0700

    Update types.ts

--- src/widgets/WalletModal/types.ts
+++ src/widgets/WalletModal/types.ts
@@ -4,7 +4,7 @@ import { SvgProps } from "../../components/Svg/types";
 export enum ConnectorNames {
   Injected = "injected",
   WalletConnect = "walletconnect",
-  BSC = "bsc",
+  ETH = "eth",
 }
 
 export type Login = (connectorId: ConnectorNames) => void;
